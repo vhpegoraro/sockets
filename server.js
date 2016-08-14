9fb2d445860a74b173f1eb7d55034358c74ae527
Victor Hugo <vh.pegoraro@gmail.com>
2016-08-14 19:54:34 -0300

    Add JS Client lib

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var socket = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+socket.on('connection', function() {
+    console.log('User connected via socket.io ...');
+});
 
 http.listen(PORT, function() {
 
