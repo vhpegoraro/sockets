6add12948ed105db1050bd23b24e13e753554468
Victor Hugo <vh.pegoraro@gmail.com>
2016-08-16 10:20:08 -0300

    Playing with timestamp

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,16 @@
 
 var socket = io();
+
+socket.on('message', function(message) {
+    
+    $("#messages").append('<p>' + message.timestamp + ' - ' + message.text + '</p>');
+});
+
+$("#message-form").on("submit", function(event) {
+
+    event.preventDefault();
+
+    socket.emit('message', $("#message").val());
+
+    $("#message").val('');
+});
--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var socket = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-socket.on('connection', function() {
-    console.log('User connected via socket.io ...');
+io.on('connection', function(socket) {
+
+    socket.emit('message', createMessage('Welcome to the Socket Chat Application'));
+
+    io.emit('message', createMessage('New user logged in !'));
+
+    socket.on('message', function(message) {
+
+        io.emit('message', message.text);    
+    });
 });
 
 http.listen(PORT, function() {
 
     console.log('Server started! Listening port '+PORT+' ...');
 });
+
+function createMessage(text) {
+
+    return {
+        text: text,
+        timestamp: moment().valueOf()
+    }
+}
